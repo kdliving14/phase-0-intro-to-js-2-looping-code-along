3f3f06e15a63f5e8f78eef635a883dde5200f5d6
Karter <kdlmuzixnerd14@gmail.com>
2022-06-29 13:58:52 -0700

    commit tree 67f8942b7e9b52b0cfe1c9c4d93396eb0cc8cdaf

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event)
+{
+    const thankMess = [];
+
+    for(let i = 0; i < names.length; i++)
+    {
+        thankMess[i]= `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return thankMess;
+}
+
+function countDown(num)
+{
+    while(num >= 0)
+    {
+        console.log(num);
+        num--;
+    }
+}
